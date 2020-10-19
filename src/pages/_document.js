4af4caaaeb9269a0786b885b2884e36618f4ceeb
Jean-Vincent QUILICHINI <jv.quilichini@jellysmack.com>
2020-10-19 20:11:20 +0200

    🚧 Mapbox searchDoc implementation

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,15 @@ export default class Document extends NextDocument {
     return { ...initialProps }
   }
 
+  componentDidMount() {
+    window.docsearch({
+      apiKey: '6ac184d35b3a04722d3b0845f50d9003',
+      indexName: 'laravelshopper',
+      inputSelector: '#docsearch-input',
+      debug: true // Set debug to true if you want to inspect the dropdown
+    });
+  }
+
   render() {
     return (
       <Html lang="en" className="bg-gray-50 antialiased">
@@ -15,12 +24,13 @@ export default class Document extends NextDocument {
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1C64F2" />
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />
+          <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script>
           <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
           <NextScript />
-          <script> </script>
         </body>
       </Html>
     )
