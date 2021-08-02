3f286b6f49facf54decff0501a4afaaf4cfa0a33
Arthur Monney <monneylobe@gmail.com>
2021-08-02 22:21:20 +0100

    :heavy_plus_sign: Add tailwind aspect-ratio plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -83,5 +83,8 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
